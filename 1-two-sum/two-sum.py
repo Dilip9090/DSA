cd68b36adb23c4c1698804685a8256690d7fc0a1
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-26 14:33:54 +0530

    Time: 9 ms (44.15%) | Memory: 13.6 MB (16.41%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,64 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        seen = {}
+    def twoSum(self, arr, target):
+        nums = []
 
-        for i, num in enumerate(nums):
-            complement = target - num
+        for i in range(len(arr)):
+            nums.append((arr[i], i))
 
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+        nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            sum1 = nums[left][0] + nums[right][0]
+
+            if sum1 == target:
+                return [nums[left][1], nums[right][1]]
+
+            elif sum1 < target:
+                left += 1
+
+            else:
+                right -= 1
+
+        return nums   
+        
+        
+        
+        
+        # mpp  = {}
+        # need = 0
+
+        # for i in range(len(arr)):
+        #     need = target - arr[i]
+        #     if need in mpp:
+        #         return [mpp[need], i]
+        #     else:
+        #         mpp[arr[i]] = i    
+
+        
+        
+        
+        
+        
+        # n = len(arr)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if arr[i] == arr[j]:
+        #             continue
+        #         if arr[i] + arr[j] == target:
+        #             return [i,j]
+
+        
+        # seen = {}
+
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+
+        #     if complement in seen:
+        #         return [seen[complement], i]
+        #     seen[num] = i
 
         # seen = {}
         
