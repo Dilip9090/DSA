aecadcecbb34008437a535d1fe4a81f821bac7ee
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-26 14:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.27%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        count0 = nums.count(0)
-        count1 = nums.count(1)
-        count2 = nums.count(2)
+        return nums.sort()
+        # count0 = nums.count(0)
+        # count1 = nums.count(1)
+        # count2 = nums.count(2)
 
-        for i in range(count0):
-            nums[i] = 0
+        # for i in range(count0):
+        #     nums[i] = 0
 
-        for i in range(count0, count0 + count1):
-            nums[i] = 1
+        # for i in range(count0, count0 + count1):
+        #     nums[i] = 1
 
-        for i in range(count0 + count1, len(nums)):
-            nums[i] = 2
+        # for i in range(count0 + count1, len(nums)):
+        #     nums[i] = 2
