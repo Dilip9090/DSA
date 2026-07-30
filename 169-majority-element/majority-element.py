5aea15f4e6ab75e88289be8f1848fd463bbfcc78
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-30 16:49:22 +0530

    Time: 31 ms (5.22%) | Memory: 13.5 MB (73.97%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,19 +4,39 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return nums[len(nums) // 2]
-        # n= len(nums)
-        # maxi = 0
-        # count = 0
+        # #1 Brute Force
+        # n = len(nums)
+        # valu = n/2
+        # for i in range(n):
+        #     count = 0
+        #     for j in range(n):
+        #         if nums[i] == nums[j]:
+        #             count +=1
+        #         if count > valu:
+        #             return nums[i]     
 
-        # for i in range (n):
-        #     if nums[i] == maxi :
-        #         count += 1
-        #         if count <= n/2:
-        #             return nums[i]
-        #     elif nums[i] != maxi:
-        #         maxi = nums[i]
+        #2 Batter
+
+        n = len(nums)
+        mpp = {}
+
+        for num in nums:
+            if num in mpp: 
+                mpp[num] += 1
+            else:
+                mpp[num] = 1
+
+            for key in mpp:
+                if mpp[key] > n//2:
+                    return key             
+
+
+
+
+        # nums.sort()
+        # return nums[len(nums) // 2]
+        
+        
 
 
         
