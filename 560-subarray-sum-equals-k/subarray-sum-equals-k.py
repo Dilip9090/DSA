8bd7d5ea4333e95970034207f493fde4a6917bb5
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-08-07 14:24:10 +0530

    Time: 33 ms (70.75%) | Memory: 14.6 MB (72.29%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,31 +1,56 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        presum = [0] * n
-        presum[0] = nums[0]
-        mpp = {0 : 1}
+class Solution(object):
+    def subarraySum(self, nums, k):
+        mpp = {0: 1}
         count = 0
+        presum  = 0
 
-        for i in range(1, n):
-            presum[i] = presum[i - 1] + nums[i]
-
-        for j in range(n):
-            val = presum[j] - k
-            if val in mpp:
-                count += mpp[val]
-            if presum[j] in mpp:
-                mpp[presum[j]] += 1
+        for num in nums:
+            presum += num
+            remove = presum - k
+            if remove in mpp:
+                count += mpp[remove]
+            if presum in mpp:    
+                mpp[presum] += 1
             else:
-                mpp[presum[j]] = 1
-        return count
-
+                mpp[presum] = 1     
+        return count    
+        
+        
+        
+        
         # n = len(nums)
         # count = 0
+        
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         sum1 = 0
 
-        # for i in range (n):
-        #     sum1 = 0
-        #     for j in range(i,n):
-        #         sum1 += nums[j]
+        #         for m in range(i, j + 1):
+        #             sum1 += nums[m]
+                
         #         if sum1 == k:
         #             count += 1
+        # return count                
+
+
+
+        # count = 0
+
+        # for i in range(len(nums)):
+        #     total = 0
+        #     for j in range(i, len(nums)):
+        #         total += nums[j]
+        #         if total == k:
+        #             count += 1
+
         # return count
+
+
+
+
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
